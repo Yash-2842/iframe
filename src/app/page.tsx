105b13b3c3741cf0ca47821a30f199023da502d3
Yash-2842 <84618410+Yash-2842@users.noreply.github.com>
2023-12-22 13:09:56 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Head from "../../node_modules/next/head";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <meta name="google-site-verification" content="vV70oydhHyHlTJtFSqdZiX6it9v1Nv_iaySUAbv0z6w" />
+      </Head>
       <h1>Next JS IFrame Project</h1>
     </div>
   );
